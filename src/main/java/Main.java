9d13816e4c7a441cc720bb6c77b42ef171023029
migueloliveiradg <miguel.d.goncalves@tecnico.ulisboa.pt>
2019-05-19 13:54:40 +0100

    Main Menu working

---
+++ src/main/java/Main.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Ring Signatures System");
+
+            while (true) {
+
+                System.out.println("Select Your Option: ");
+                System.out.println("1 - Sign message");
+                System.out.println("2 - Verify signature");
+                System.out.println("3 - Exit");
+                System.out.println("Insert option here: ");
+
+                String option = reader.readLine();
+
+                while (!tryParseInt(option)) {
+                    System.out.println("Please Introduce only the Number of The Option You Select:");
+                    option = reader.readLine();
+                }
+
+                switch (Integer.valueOf(option)) {
+                    case 1:
+                        System.out.println("1 selected");
+                        break;
+                    case 2:
+                        System.out.println("2 selected");
+                        break;
+                    case 3:
+                        System.out.println("Exiting...");
+                        System.exit(0);
+                    default:
+                        System.out.println("Invalid option");
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    private static boolean tryParseInt(String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (Exception e) {
+            System.out.println("The introduced Input could not be converted to an integer.");
+            return false;
+        }
+    }
+}
